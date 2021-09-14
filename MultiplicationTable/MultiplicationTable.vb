b3c574867d7379ac35c8ff10bbdc86accb1a00bf
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-13 23:19:52 -0600

    end for night

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -8,33 +8,48 @@ Option Explicit On
 Option Compare Text
 Module MultiplicationTable
     Sub Main()
-        Dim one As Integer = 1
-        Dim firstNumber As Integer
-        Dim secondNumber As Integer
-        Dim multiNum As Integer
-        Dim numOne As String
-        Dim numTwo As String
 
+        'Dim numTwo As String
+        ''numTwo = Console.ReadLine()
+        'secondNumber = CInt(numTwo)
+        'multiNumtwo = 1 * multiNumtwo
+        'Console.WriteLine($"{multiNumtwo} ")
+        'multiNumtwo = multiNumtwo + 1
+        'Console.ReadLine()
+        'Left()
+
+        Dim firstNumber As Integer
+        Dim multiNum As Integer = 1
+        Dim multiNumTwo As Integer = 1
+        Dim numOne As String
+        Dim lineCheck As Integer
         Console.WriteLine("Num one is for loop and num two is num in loop")
         numOne = Console.ReadLine()
         firstNumber = CInt(numOne)
-        numTwo = Console.ReadLine()
-        secondNumber = CInt(numTwo)
-
 
 
         For i = 1 To firstNumber
-            multiNum = (1 * multiNum)
+            multiNum = 1 * multiNum
 
-            Console.Write($"{multiNum} ,")
+            Console.Write($"{multiNum} ")
 
-            multiNum = multiNum + 1
+            multiNum = 1 + multiNum
+        Next
+
+        For i = 1 To firstNumber
+            multiNumTwo = 1 * multiNumTwo
 
 
+            Console.WriteLine($"{multiNumTwo} ")
+
+            multiNumTwo = 1 + multiNumTwo
         Next
 
         Console.ReadLine()
+    End Sub
+    Sub Left()
+
+
 
     End Sub
-
 End Module
