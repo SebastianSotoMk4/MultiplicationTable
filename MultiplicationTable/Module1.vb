f40c7240953a9bb4f28250addc5f7f803b8332d3
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-13 22:16:35 -0600

    Add project files.

---
+++ MultiplicationTable/Module1.vb
@@ -0,0 +1,15 @@
+﻿Sebastian Soto
+'RCET0265
+'Fall 2021
+'BetterCalculator
+'https://github.com/SebastianSotoMk4/BetterCalculator.git
+Option Strict On
+Option Explicit On
+Option Compare Text
+Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
