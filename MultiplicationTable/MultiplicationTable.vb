98ef3c58174045cced0f47e150df094f7c830b3f
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-18 14:20:21 -0600

    added padding to numbers

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -12,7 +12,7 @@ Module MultiplicationTable
         Dim storedNumber As Integer
         Dim userInput As String
         Dim numberCheck As Boolean
-
+        Dim printNumber As String
         Do
             Console.WriteLine("Enter a number:")
             userInput = Console.ReadLine()
@@ -23,14 +23,11 @@ Module MultiplicationTable
                 Console.WriteLine("Please enter a number")
             End Try
         Loop Until numberCheck = True
-
         Do Until storedNumber = userNumber
-
             storedNumber = 1 + storedNumber
             For i As Integer = 1 To userNumber
-
-                Console.Write("{0} ", i * storedNumber)
-
+                printNumber = CStr(i * storedNumber).PadLeft(4)
+                Console.Write(printNumber)
             Next
             Console.WriteLine()
         Loop
